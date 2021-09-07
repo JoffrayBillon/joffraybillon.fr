0c1c3aaf12ab36778d65ec07ee3812ae8acf923f
Joffray Billon <j.billon@groupeonepoint.com>
2021-09-07 12:24:24 +0200

    Latest changes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ import image2 from "./assets/photo.jpg";
 import image3 from "./assets/gaming.jpg";
 import image4 from "./assets/pro.jpg";
 
+const time = 700;
+
 export default function App() {
   const [image, setImage] = useState(image1);
+  const [action, setAction] = useState(null);
   const changeImage = (i) => {
-    setImage(i);
+    if (!action) setImage(i);
+  };
+
+  const redirectAfterHide = (e) => {
+    e.preventDefault();
+    setAction(e);
   };
 
   const transitions = useTransition(image, {
@@ -19,10 +27,23 @@ export default function App() {
     enter: { opacity: 1 },
     leave: { opacity: 0 },
     config: {
-      duration: 1000,
+      duration: 500,
     },
   });
 
+  useEffect(() => {
+    let timeout;
+    if (action) {
+      timeout = setTimeout(() => {
+        window.location = action.target.href;
+      }, time);
+    }
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [action]);
+
+  // ? Preload the images to avoid flash on first render
   useEffect(() => {
     const pictures = [image1, image2, image3, image4];
     pictures.forEach((picture) => {
@@ -32,7 +53,7 @@ export default function App() {
   }, []);
 
   return (
-    <div>
+    <>
       {transitions((props, item) => {
         return (
           <animated.div
@@ -44,21 +65,24 @@ export default function App() {
       <div className="box">
         <div className="innerBox">
           <a
-            href="./#"
+            href="https://photo.joffraybillon.fr"
+            onClick={redirectAfterHide}
             onMouseOver={() => changeImage(image2)}
             onMouseOut={() => changeImage(image1)}
           >
             Photography
           </a>
           <a
-            href="./#"
+            href="https://freedou.fr"
+            onClick={redirectAfterHide}
             onMouseOver={() => changeImage(image3)}
             onMouseOut={() => changeImage(image1)}
           >
             Streaming
           </a>
           <a
-            href="./#"
+            href="https://joffraybillon.fr"
+            onClick={redirectAfterHide}
             onMouseOver={() => changeImage(image4)}
             onMouseOut={() => changeImage(image1)}
           >
@@ -66,6 +90,6 @@ export default function App() {
           </a>
         </div>
       </div>
-    </div>
+    </>
   );
 }
